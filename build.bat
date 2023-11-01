ca1f22b4e7aba72bfa4adce7094793b3f117a3fb
Hamano0813 <hamano0813@gmail.com>
2023-11-01 16:13:05 +0800

    更改文件名称

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ for /f "delims=" %%i in ('dir /b *.pyd') do (
     del %%i
 )
 
-python build.py build_ext --inplace
+python setup.py build_ext --inplace
 
 for /f "delims=" %%i in ('dir /b *.pyd') do (
     for /f "tokens=1 delims=." %%a in ("%%i") do (
